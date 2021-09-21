3846cf0c009e7beeba635d39376329c4fcab5860
Dan Lo <nflwwe71@gmail.com>
2021-09-21 11:18:18 -0400

    Made Css changes, added photos folder, adding functionality for image population

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ import React, { Component } from 'react';
 import CompletedList from './CompletedList';
 import toDoList from './data';
 import Weather from './Weather';
+
 import Tasks from './Tasks';
 console.table(toDoList)
 
@@ -116,54 +117,59 @@ class App extends Component {
 
     let newList = this.state.toDoList.filter(item => item.subject !== task.subject)
     this.setState({
-      toDoList:newList
-    
+      toDoList: newList
+
     })
 
   };
-  strikeThrough = (event) =>{
-        
-    
+  strikeThrough = (event) => {
+
+
     // delete element
     // element.classList.toggle('strike')
     //I need to allow the strike through to only strike through the element that we clicked on
     //Already kickin on the location. want thatc cto chhnage to the actual value
-    
-}
+
+  }
   render() {
     return (
       <div className="app"><div className="title"><h2>Welcome to Your Perosnal Reminder App</h2>
-            <p>To-Do List!</p></div>
+        <p>To-Do List!</p></div>
         <div className="container">
-          
 
-          <div className="form">
-            <form onSubmit={this.handleSubmit}>
-              <label htmlFor="subject">Subject:</label>
-              <input type="text" value={this.state.subject} onChange={this.handleChange} id="subject" />
-              <br />
+          <div className="container2">
+            
+            <Weather />
+            
+            <div className="form">
+              <form onSubmit={this.handleSubmit}>
+                <label htmlFor="subject">Subject:</label>
+                <input type="text" value={this.state.subject} onChange={this.handleChange} id="subject" />
+                <br />
 
-              <label htmlFor="date">Date</label>
-              <input type="text" id="date" value={this.state.date} onChange={this.handleChange} />
-              <br />
+                <label htmlFor="date">Date</label>
+                <input type="text" id="date" value={this.state.date} onChange={this.handleChange} />
+                <br />
 
-              <label htmlFor="notes">Notes</label>
-              <input type="text" id="notes" value={this.state.notes}  onChange={this.handleChange} placeholder="Write Notes In" />
+                <label htmlFor="notes">Notes</label>
+                <input type="text" id="notes" value={this.state.notes} onChange={this.handleChange} placeholder="Write Notes In" />
 
-              <br />
-              <input type="submit" />
-            </form>
+                <br />
+                <input type="submit" />
+              </form>
+            </div>
+
+
           </div>
-          
-          </div>
-          <div className="showsTypingOnScreen">
-            <h1>New Item being Rendered </h1>
-            {this.state.subject}
-            {this.state.date}
-            {this.state.notes}
-          </div>
-          <Weather />
-          <h3>Task to Complete</h3>
+        </div>
+        <div className="showsTypingOnScreen">
+          <h1>New Item being Rendered </h1>
+          {this.state.subject}
+          {this.state.date}
+          {this.state.notes}
+        </div>
+
+        <h3>Task to Complete</h3>
         <div className="container2">
           <div className="list">{
             this.state.toDoList.map((task, index) => {
@@ -177,10 +183,10 @@ class App extends Component {
                 // <ProductList  {...this.state.products}/>
                 //  Each element in the products array is its own object
                 // Every child in an array needs a UNIQUE ID
-              
-                  <Tasks key={index}  handleAdd={this.addToTask} task={task}  />
-              
-                
+
+                <Tasks key={index} handleAdd={this.addToTask} task={task} />
+
+
               )
             })
           }
--- src/Weather.js
+++ src/Weather.js
@@ -11,7 +11,7 @@ const api = "da6e6c86218ce008759316044bf839e6"
 class Weather extends Component {
     state = {
         data: '',
-        days:[1,2,3,4,5,6,7]
+        days:['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
     }
     componentDidMount() {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`)
@@ -19,13 +19,25 @@ class Weather extends Component {
             .then(data => this.setState({ data: data }))
             .catch(err => console.log(err))
     }
+
     render() {
         console.log(this.state.data.daily)
-        const renderData = this.state.data ?  this.state.data.daily.slice(1,8).map((day,index) => {
+        const renderData = this.state.data ?  this.state.data.daily.`slice(1,8).map((day,index) => {
             console.log("We are in Weather" , day)
+            if (day.weather[0].main =="cloudy") {
+                <img src="./photos/cloudy.jpg"></img>
+            } else if(day.weather[0].main =="sunny") {
+                
+            }
+            else if(day.weather[0].main =="rain") {
+                
+            }
+            else if(day.weather[0].main =="clear") {
+                
+            }
             return (
                 <div key={index}>
-                    
+
                 <h3>{this.state.days[index]}</h3>: {day.weather[0].main}
                 
                 </div>
@@ -33,7 +45,8 @@ class Weather extends Component {
         }):null
         return (
 
-            <div>
+            <div className="container3">
+                
                 {renderData}
             </div>
         );
